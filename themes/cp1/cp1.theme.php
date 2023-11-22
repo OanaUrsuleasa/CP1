25b7db0752c08d48ab20d2e94b844e062c519aba
Oana Ursuleasa <oana.ursuleasa3199@gmail.com>
2023-11-22 17:36:12 +0200

    initial commit

---
+++ themes/cp1/cp1.theme.php
@@ -0,0 +1,7 @@
+<?php
+
+function cp1_preprocess_page(&$variables) {
+  $variables['january_info_link_phone']="02323232";
+  $variables['january_info_link_email']= "email@domain.tld";
+  $variables['january_info_link_address']= "123 Abc Street, Manchester, NH 64 ABC Street, Suite 302, Concord, VO03301 10 Xyz Street, AU";
+}
